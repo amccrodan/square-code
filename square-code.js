f85cb4167be1f4ed326a7ffccf78346efff1ca27
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-11 23:12:46 +0000

    initial commit, created square-code.js

---
+++ square-code.js
@@ -0,0 +1,13 @@
+var inputArg = process.argv[2];
+
+function squareCode(inputSentence) {
+  if (!inputSentence || inputSentence.length > 81) {
+    return "Please enter a sentence between 1 and 81 characters!"
+  }
+
+  var outputSentence = "";
+
+  return outputSentence;
+}
+
+console.log(squareCode(inputArg));
