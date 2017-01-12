d36fba66215ee8025081e54d74a08acbfe014809
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 00:05:27 +0000

    implemented printout logic with hardcoded number of cols

--- square-code.js
+++ square-code.js
@@ -1,13 +1,37 @@
 var inputArg = process.argv[2];
 
 function squareCode(inputSentence) {
-  if (!inputSentence || inputSentence.length > 81) {
-    return "Please enter a sentence between 1 and 81 characters!"
+  if (!inputSentence || inputSentence.length > 81 || inputSentence.search(" ") !== -1) {
+    return "Please enter a sentence with no spaces between 1 and 81 characters!"
   }
 
   var outputSentence = "";
+  var cols = 3;
+
+  for (var i = 0; i < cols; i++) {
+    var word = "";
+    for (var j = i; j < inputSentence.length; j+= cols) {
+      word += inputSentence[j];
+    }
+    outputSentence += word + " ";
+  }
 
   return outputSentence;
+}
+
+// determines closest square number under input string length
+function getLowerSquare(numChars) {
+  return Math.floor(Math.sqrt(numChars));
+}
+
+// determine the number of columns to use
+function getNumCols(numChars) {
+  var lowerSquare = getLowerSquare(numChars);
+  if (Math.floor(numChars / lowerSquare) > lowerSquare) {
+    return lowerSquare + 1;
+  } else {
+    return lowerSquare;
+  }
 }
 
 console.log(squareCode(inputArg));
